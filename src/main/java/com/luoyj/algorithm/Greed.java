7c83774d378e0f4950c37db7646e7d6467e11dd8
DbReseter <52435263+DbReset@users.noreply.github.com>
2021-05-10 14:18:31 +0800

    添加贪心算法例子

--- src/main/java/com/luoyj/algorithm/Greed.java
+++ src/main/java/com/luoyj/algorithm/Greed.java
@@ -6,19 +6,20 @@ import java.util.Arrays;
 //贪心算法
 public class Greed {
     //经典 找零钱
-    public static void ChangeMoney(int  money){
+    public static void ChangeMoney(int  money) {
 
-        int[] moneybox = {100,50,20,10,5,1};
+        int[] moneybox = {100, 50, 20, 10, 5, 1};
         int[] changArray = new int[moneybox.length];
-        System.out.println(Arrays.toString(changArray));
+        Arrays.sort(moneybox);
+        //  System.out.println(Arrays.toString(changArray));
         int change = money;
-        if(money > 0){
+        if (money > 0) {
 
-            while(change > 0){
+            while (change > 0) {
 
-                for(int i=0;i<moneybox.length;i++){
+                for (int i = moneybox.length - 1; i >= 0; i--) {
 
-                    for(int j=1;change - moneybox[i] >= 0;j++) {
+                    for (int j = 1; change - moneybox[i] >= 0; j++) {
                         int tmp = change - moneybox[i];
                         if (tmp >= 0) {
                             change = tmp;
@@ -27,25 +28,19 @@ public class Greed {
                             break;
                         }
                     }
-
-
-
                 }
 
 
             }
 
 
-
         }
 
-        for(int p=0;p<moneybox.length;p++){
-            if(changArray[p] > 0){
-                System.out.println("需要找零"+moneybox[p]+" "+changArray[p]+"张");
+        for (int p = 0; p < moneybox.length; p++) {
+            if (changArray[p] > 0) {
+                System.out.println("需要找零" + moneybox[p] + " " + changArray[p] + "张");
             }
         }
-
-
 
 
     }
